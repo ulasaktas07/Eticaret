8442e025bd7c0aaca62e460afa60462f24d9b088
Ulaş Aktaş <ulasaktas882@gmail.com>
2025-04-11 17:35:01 +0300

    Generic Repository Pattern

--- Eticaret.WebUI/Controllers/AccountController.cs
+++ Eticaret.WebUI/Controllers/AccountController.cs
@@ -4,7 +4,6 @@ using Eticaret.WebUI.Models;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using System.Security.Claims;
 
 namespace Eticaret.WebUI.Controllers
